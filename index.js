b8435491b1c81269d41bf0218352ae1db175594d
Your Name <you@example.com>
2022-09-08 23:30:34 +0800

    feat() remove regexp

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-var glob = require('glob');
-var fs = require('fs-extra');
-var path = require('path');
+const glob = require('glob');
+const fs = require('fs-extra');
+const path = require('path');
 const options = { nodir: true }
-var replaceConfig = path.resolve('replace.json');
+const replaceConfig = path.resolve('replace.json');
 console.log(replaceConfig);
 let replace = {};
 if (fs.existsSync(replaceConfig)) {
-    replace = fs.readJsonSync(replaceConfig);
+    const content = fs.readFileSync(replaceConfig, 'utf-8');
+    replace = JSON.parse(content);
 } else {
     throw new Error(`在当前目录下没有到到replace.json配置文件!`);
 }
@@ -17,7 +18,11 @@ glob('./**', options, function (err, files) {
     Promise.all(files.map(file => {
         let newFile = file;
         for (let key in replace) {
-            newFile = newFile.replace(new RegExp(key, 'g'), replace[key]);
+            let before;
+            do {
+                before = newFile;
+                newFile = newFile.replace(key, replace[key]);
+            } while (before !== newFile)
         }
         if (file !== newFile) {
             console.log(file, '=>', newFile);
